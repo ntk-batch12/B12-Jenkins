a04a0985b0dd533c57c8c0b4d63b20c2dd96f34b
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-21 20:12:27 -0400

    bb

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -29,12 +29,13 @@ public class LoginStep {
 //		
 		
 		//Firfox Window User 
-	//	WebDriverManager.firefoxdriver().setup();
-		//driver = new FirefoxDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		
 		//Edge-WIndows User
- 		WebDriverManager.edgedriver().setup();
- 		driver = new EdgeDriver();
+ 		//WebDriverManager.edgedriver().setup();
+ 		
+ 		//driver = new EdgeDriver();
 		
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
